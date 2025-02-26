806248309bb7a0712c83e3fd31d36bf7b351871c
Alexander Svobodov <31383841+sidecuter@users.noreply.github.com>
2025-02-26 21:07:42 +0300

    Add nixosModule with configuration for maibenben p477

--- packages/default.nix
+++
@@ -1,3 +0,0 @@
-{pkgs}: let
-  kernel_modules = import ./kernel-modules { inherit pkgs; };
-in {} // kernel_modules
--- packages/kernel-modules/default.nix
+++
@@ -1,5 +0,0 @@
-{pkgs}: {
-  skr = pkgs.linuxPackages.callPackage ./skr {};
-  skr_zen = pkgs.linuxPackages_zen.callPackage ./skr {};
-  skr_latest = pkgs.linuxPackages_latest.callPackage ./skr {};
-}
